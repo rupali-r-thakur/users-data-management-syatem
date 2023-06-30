56de9c9df1975d28ab8c703531c98d6d191e52ba
rupali-r-thakur <rupali97097@gmail.com>
2023-06-30 16:15:29 +0530

    gating user's data from api

--- src/App.js
+++ src/App.js
@@ -1,155 +1,49 @@
-import { useState } from "react";
-import {Link, Routes, Route} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import UserList from "./UserList";
-import Create from "./Create"
 function App() {
-  const users = [
-    {
-      name: "John Doe",
-      email: "john.doe@example.com",
-      salary: 50000,
-      address: "123 Main St, City, State",
-    },
-    {
-      name: "Jane Smith",
-      email: "jane.smith@example.com",
-      salary: 60000,
-      address: "456 Elm St, City, State",
-    },
-    {
-      name: "Alex Johnson",
-      email: "alex.johnson@example.com",
-      salary: 55000,
-      address: "789 Oak St, City, State",
-    },
-    {
-      name: "Emily Davis",
-      email: "emily.davis@example.com",
-      salary: 70000,
-      address: "321 Pine St, City, State",
-    },
-    {
-      name: "Michael Wilson",
-      email: "michael.wilson@example.com",
-      salary: 45000,
-      address: "987 Maple St, City, State",
-    },
-    {
-      name: "Sarah Thompson",
-      email: "sarah.thompson@example.com",
-      salary: 55000,
-      address: "654 Cedar St, City, State",
-    },
-    {
-      name: "Christopher Brown",
-      email: "christopher.brown@example.com",
-      salary: 60000,
-      address: "789 Elm St, City, State",
-    },
-    {
-      name: "Olivia Taylor",
-      email: "olivia.taylor@example.com",
-      salary: 55000,
-      address: "123 Oak St, City, State",
-    },
-    {
-      name: "Matthew Anderson",
-      email: "matthew.anderson@example.com",
-      salary: 50000,
-      address: "456 Pine St, City, State",
-    },
-    {
-      name: "Ava Martinez",
-      email: "ava.martinez@example.com",
-      salary: 65000,
-      address: "789 Cedar St, City, State",
-    },
-    {
-      name: "David Thompson",
-      email: "david.thompson@example.com",
-      salary: 55000,
-      address: "321 Maple St, City, State",
-    },
-    {
-      name: "Sophia Martin",
-      email: "sophia.martin@example.com",
-      salary: 55000,
+  const [filterValue, setFilterValue] = useState([]);
 
-      address: "654 Elm St, City, State",
-    },
-    {
-      name: "William Rodriguez",
-      email: "william.rodriguez@example.com",
-      salary: 60000,
-      address: "987 Oak St, City, State",
-    },
-    {
-      name: "Isabella Hernandez",
-      email: "isabella.hernandez@example.com",
-      salary: 55000,
-      address: "123 Pine St, City, State",
-    },
-    {
-      name: "James Lee",
-      email: "james.lee@example.com",
-      salary: 55000,
-      address: "456 Cedar St, City, State",
-    },
-    {
-      name: "Emma Clark",
-      email: "emma.clark@example.com",
-      salary: 55000,
-      address: "789 Maple St, City, State",
-    },
-    {
-      name: "Benjamin Walker",
-      email: "benjamin.walker@example.com",
-      salary: 59000,
-      address: "789 Maple , City, State",
-    },{
-      name: "James ",
-      email: "james.Jameslee@example.com",
-      salary: 95000,
-      address: "756 Cedar St, City, State",
-    },
-    {
-      name: " Clark",
-      email: "Clark.clark@example.com",
-      salary: 75000,
-      address: "589 Maple St, City, State",
-    },
-    {
-      name: " Walker",
-      email: "Walker.walker@example.com",
-      salary: 53000,
-      address: "769 Maple , City, State",
-    },{
-      name: " Lee",
-      email: "Lee.lee@example.com",
-      salary: 50000,
-      address: "450 Cedar St, City, State",
-    },
-    {
-      name: " Clark",
-      email: "Clark.clark@example.com",
-      salary: 75000,
-      address: "789 Maple , City, State",
-    },
-    {
-      name: "Benjamin ",
-      email: "benj.walker@example.com",
-      salary: 59000,
-      address: "729 Maple , City, State",
-    },
-  ];
+  const [currentPage, setCurrentPage] = useState(1);
+  const [limitData, setLimitData] = useState(10);
+  const [total, setTotal] = useState(0);
+
+  const getUsers = async () => {
+    try {
+      console.log("calling", (currentPage - 1) * limitData);
+      const usersData = await fetch(
+        ` https://dummyjson.com/users?limit=${limitData}&skip=${
+          (currentPage - 1) * limitData
+        }`
+      ).then((res) => res.json());
+      console.log(usersData);
+      setTotal(usersData.total / limitData);
+      // console.log(usersData.users[0].firstName);
+      setFilterValue(usersData.users);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [currentPage]);
   return (
     <>
-    <Routes>
-      <Route path="/" element={<UserList users={users}/>} />
-
-      <Route path="/create" element={<Create users={users}/>}/>
-    </Routes>
-      
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <UserList
+              data={filterValue}
+              setFilterValue={setFilterValue}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+              setTotal={setTotal}
+              total={total}
+              limitData={limitData}
+            />
+          }
+        />
+      </Routes>
     </>
   );
 }
--- src/UserList.jsx
+++ src/UserList.jsx
@@ -1,42 +1,88 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-function UserList({users}){
-    
-  const [filterValue, setFilterValue] = useState(users);
+function UserList({
+  data,
+  currentPage,
+  setCurrentPage,
+  setTotal,
+  total,
+  limitData,
+}) {
+  const [searchByName, setSearchByName] = useState();
+  const [searchByEmail, setSearchByEmail] = useState();
+  const [searchByAddress, setSearchByAddress] = useState();
+  const [searchByAge, setSearchByAge] = useState();
+  const [filterValue, setFilterValue] = useState(data);
+  console.log(filterValue);
+  const prevPage = () => {
+    if(currentPage>1){
+      console.log("hello");
+      console.log(currentPage);
+      setCurrentPage(currentPage - 1);
+    }
+   
+  };
+  const nextPage = () => {
+    if(currentPage<total){
+    setCurrentPage(currentPage + 1);}
+  };
+  const btn = (num) => {
+    setCurrentPage(num);
+  };
   let filterArray = [];
   const inputValue = (event) => {
     let valueOfInput = event.target.value.toUpperCase();
     switch (event.target.name) {
       case "searchName":
-        filterArray = users.filter((element) => {
-          return element.name.toUpperCase().includes(valueOfInput);
+        setSearchByName(event.target.value);
+        setSearchByEmail("");
+        setSearchByAddress("");
+        setSearchByAge("");
+        filterArray = data.filter((element) => {
+          console.log(element);
+          return element.firstName.toUpperCase().includes(valueOfInput);
         });
         setFilterValue(filterArray);
         break;
       case "searchEmail":
-        filterArray = users.filter((element) => {
+        setSearchByName("");
+        setSearchByEmail(event.target.value);
+        setSearchByAddress("");
+        setSearchByAge("");
+        filterArray = data.filter((element) => {
           return element.email.toUpperCase().includes(valueOfInput);
         });
         setFilterValue(filterArray);
         break;
       case "searchAddress":
-        filterArray = users.filter((element) => {
-          return element.address.toUpperCase().includes(valueOfInput);
+        setSearchByName("");
+        setSearchByEmail("");
+        setSearchByAddress(event.target.value);
+        setSearchByAge("");
+        filterArray = data.filter((element) => {
+          return element.address.address.toUpperCase().includes(valueOfInput);
         });
         setFilterValue(filterArray);
         break;
-      case "searchSalary":
-        filterArray = users.filter((element) => {
-          return element.salary > event.target.value;
+      case "searchAge":
+        setSearchByName("");
+        setSearchByEmail("");
+        setSearchByAddress("");
+        setSearchByAge(event.target.value);
+        filterArray = data.filter((element) => {
+          return element.age >= event.target.value;
         });
         setFilterValue(filterArray);
         break;
       default:
     }
   };
-    return (
-        <>
-        <div className="header">
+  useEffect(() => {
+    setFilterValue(data);
+  }, [data]);
+  return (
+    <>
+      <div className="header">
         <h1>users-data-management-system</h1>
       </div>
       <div className="input-container">
@@ -47,6 +93,7 @@ function UserList({users}){
             id="formControlReadonly"
             name="searchName"
             type="text"
+            value={searchByName}
             onChange={inputValue}
             placeholder="search-name"
           />
@@ -59,6 +106,7 @@ function UserList({users}){
             type="text"
             onChange={inputValue}
             name="searchEmail"
+            value={searchByEmail}
             placeholder="search-email"
           />
         </div>
@@ -70,6 +118,7 @@ function UserList({users}){
             type="text"
             onChange={inputValue}
             name="searchAddress"
+            value={searchByAddress}
             placeholder="search-address"
           />
         </div>
@@ -80,16 +129,11 @@ function UserList({users}){
             id="formControlReadonly"
             type="text"
             onChange={inputValue}
-            name="searchSalary"
-            placeholder="search-salary"
+            name="searchAge"
+            value={searchByAge}
+            placeholder="search-age"
           />
         </div>
-        <Link to="/create"> <button
-          type="button"
-          class="btn btn-secondary"
-        >
-          Secondary
-        </button></Link>
       </div>
       <table class="table table-hover" style={{ width: "100%" }}>
         <thead>
@@ -98,23 +142,52 @@ function UserList({users}){
             <th scope="col">name</th>
             <th scope="col">email</th>
             <th scope="col">address</th>
-            <th scope="col">salary</th>
+            <th scope="col">age</th>
           </tr>
         </thead>
         <tbody>
           {filterValue.map((element, index) => (
             <tr>
-              <th scope="row">{index + 1}</th>
-              <td>{element.name}</td>
+              <th scope="row">{(currentPage - 1) * limitData + index + 1}</th>
+              <td>{element.firstName}</td>
               <td>{element.email}</td>
-              <td>{element.address}</td>
-              <td>{element.salary}</td>
+              <td>{element.address.address}</td>
+              <td>{element.age}</td>
             </tr>
           ))}
         </tbody>{" "}
       </table>
-        
-        </>
-    )
+
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+          <li role="button" class="page-item disabled" onClick={prevPage}>
+            <a class="page-link" tabindex="-1">
+              Previous
+            </a>
+          </li>
+          {Array.from(Array(total), (e, inx) => {
+            return (
+              <li role="button" class="page-item">
+                <a
+                  class={
+                    inx + 1 == currentPage ? "page-link active" : "page-link"
+                  }
+                  onClick={() => {
+                    btn(inx + 1);
+                  }}
+                >
+                  {inx + 1}
+                </a>
+              </li>
+            );
+          })}
+
+          <li role="button" class="page-item" onClick={nextPage}>
+            <a class="page-link">Next</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
-export default UserList
+export default UserList;
